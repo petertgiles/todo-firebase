51ce8afb066a12bc975b6f46d71ccac3348a7be5
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-07-19 21:02:56 -0400

    firebase install firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
 function App() {
   const [count, setCount] = useState(0);
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAKGv9ycTm2-HcP20gHPrKbJPVQAa_nxh4",
-    authDomain: "petertgiles-todo.firebaseapp.com",
-    projectId: "petertgiles-todo",
-    storageBucket: "petertgiles-todo.firebasestorage.app",
-    messagingSenderId: "982994779941",
-    appId: "1:982994779941:web:ef36b1835a800f5fda64cc",
-  };
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyAKGv9ycTm2-HcP20gHPrKbJPVQAa_nxh4",
+  //   authDomain: "petertgiles-todo.firebaseapp.com",
+  //   projectId: "petertgiles-todo",
+  //   storageBucket: "petertgiles-todo.firebasestorage.app",
+  //   messagingSenderId: "982994779941",
+  //   appId: "1:982994779941:web:ef36b1835a800f5fda64cc",
+  // };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  // const app = initializeApp(firebaseConfig);
 
   return (
     <>
