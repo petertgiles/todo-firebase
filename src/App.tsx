645c31f491adca015875fd4d5f45deaa5f894e37
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-07-20 14:29:27 -0400

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,52 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
   const [count, setCount] = useState(0);
 
   // Your web app's Firebase configuration
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyAKGv9ycTm2-HcP20gHPrKbJPVQAa_nxh4",
-  //   authDomain: "petertgiles-todo.firebaseapp.com",
-  //   projectId: "petertgiles-todo",
-  //   storageBucket: "petertgiles-todo.firebasestorage.app",
-  //   messagingSenderId: "982994779941",
-  //   appId: "1:982994779941:web:ef36b1835a800f5fda64cc",
-  // };
+  const firebaseConfig = {
+    apiKey: "AIzaSyAKGv9ycTm2-HcP20gHPrKbJPVQAa_nxh4",
+    authDomain: "petertgiles-todo.firebaseapp.com",
+    projectId: "petertgiles-todo",
+    storageBucket: "petertgiles-todo.firebasestorage.app",
+    messagingSenderId: "982994779941",
+    appId: "1:982994779941:web:ef36b1835a800f5fda64cc",
+  };
 
   // Initialize Firebase
-  // const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+
+  const auth = getAuth();
+
+  const tryLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.debug("success!", user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.debug("failure!", error);
+      });
+  };
 
   return (
     <>
@@ -42,6 +71,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button type="button" onClick={tryLogin}>
+        Try login
+      </button>
     </>
   );
 }
