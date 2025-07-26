3f7bd1f2d5af8625ee2c6ec815b0352f201a8516
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-07-25 21:32:07 -0400

    use npm react error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import FirebaseAppProvider from "./providers/firebase/FirebaseAppProvider";
 import FirebaseAuthProvider from "./providers/firebase/FirebaseAuthProvider";
 import Header from "./components/Header";
 import ItemList from "./components/ItemList";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
+
+function fallbackRender({ error }: FallbackProps) {
+  console.debug("fallbackRender", error);
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error}</pre>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <ErrorBoundary>
+    <ErrorBoundary fallbackRender={fallbackRender}>
       <FirebaseAppProvider>
         <FirebaseAuthProvider>
           <div className="flex flex-col">
