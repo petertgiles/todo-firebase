7910035f76fb62a6fd630c61009cb71221b4827b
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-08-30 17:18:35 -0400

    basic working admin user

--- admin/admin-users.ts
+++ admin/admin-users.ts
@@ -1,14 +1,27 @@
 #!/usr/bin/env tsx
 
 import * as dotenv from 'dotenv'
-import { getFirestore } from "firebase-admin/firestore";
 import { initAdmin } from './firebaseAdmin';
+import {getAuth} from "firebase-admin/auth";
 
 dotenv.config({path: "admin/.env"});
 
  await initAdmin();
-  const firestore = getFirestore();
-  const listItemsSnapshot = await firestore.collection("list_items").get();
-  listItemsSnapshot.docs.forEach((doc) => {
-    console.debug(JSON.stringify(doc.data()));
-  });
+
+  getAuth()
+    .getUserByEmail('')
+    .then((user) => {
+      
+      console.debug(JSON.stringify(user, null, 2))
+
+        // Add incremental custom claim without overwriting existing claims.
+    const currentCustomClaims = user.customClaims ?? {};
+    
+      // Add level.
+      currentCustomClaims['app_user'] = true;
+      // Add custom claims for additional privileges.
+      return getAuth().setCustomUserClaims(user.uid, currentCustomClaims);
+    
+
+    })
+    .catch((reason) => console.error(`Failure: ${reason}`));
